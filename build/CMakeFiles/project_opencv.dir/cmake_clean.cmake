368e3866702e46e7a730f010e373098d4becdde1
Natan Lotério <natanloterio@gmail.com>
2021-02-24 17:01:07 -0300

    initial commit

---
+++ build/CMakeFiles/project_opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_opencv.dir/main.cpp.o"
+  "project_opencv"
+  "project_opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
